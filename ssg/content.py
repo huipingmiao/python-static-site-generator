9248c967a016cbb707de5ab62d24f939cea452ac
Huiping Miao <Huiping.Miao@sas.com>
2023-04-25 14:09:50 -0400

    module 3

--- ssg/content.py
+++ ssg/content.py
@@ -3,8 +3,8 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = r"^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
@@ -20,6 +20,10 @@ class Content(Mapping):
     def body(self):
         return self.data["content"]
 
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+    
     @type.setter
     def type(self, type):
         self.data["type"] = type
